755ee0722d0b209e019978c17c8f383544909594
tockertheo <theodor.hoefer@gmail.com>
2024-06-04 14:42:35 +0200

    generating buildings
    die ressourcen müssen noch irgendwo hin

---
+++ MarsColonyGame/buildings/biodome.py
@@ -0,0 +1,8 @@
+from buildings.generating_buildings import GeneratingBuildings
+
+class Biodome:
+    def __init__(self) -> None:
+        super().__init__()
+        self.ressource_type = "food"
+    
+
---
+++ MarsColonyGame/buildings/ore_mine.py
@@ -0,0 +1,7 @@
+from buildings.generating_buildings import GeneratingBuildings
+
+class Ore_mine:
+    def __init__(self) -> None:
+        super().__init__()
+        self.ressource_type = "ore"
+    
